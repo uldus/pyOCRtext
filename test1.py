b9a2fd205181e9fdd4d3bb03c8650d682a3405e1
uldus <uldus@mail.ru>
2021-11-15 16:08:51 +0300

    2021-11-15 16:08 update

--- test1.py
+++ test1.py
@@ -1,7 +1,17 @@
-
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def main():
-    print('Hello World!')
+    # читать изображение с помощью OpenCV
+    image = cv2.imread(r'''C:\Users\galimov\PycharmProjects\pyOCRtext1\temp\test.png''')
+    # или вы можете использовать подушку
+    #image = Image.open('C:/Users/galimov/PycharmProjects/pyOCRtext1/temp/test1.png')
+    # получаем строку
+    string = pytesseract.image_to_string(image,lang = 'eng')
+    # печатаем
+    print(string)
 
 if __name__ == '__main__':
     main()
