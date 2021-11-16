1c9b2722ade194c1779745d233c9ad943e35c516
uldus <uldus@mail.ru>
2021-11-16 15:39:59 +0300

    2021-11-16 15:39 update

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import pytesseract
 import cv2
+import os
 import matplotlib.pyplot as plt
 from PIL import Image
 
@@ -12,13 +13,39 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 def main():
     # читать изображение с помощью OpenCV
-    image = cv2.imread(r'''temp\test3.png''')
-    # или вы можете использовать подушку
-    #image = Image.open('C:/Users/galimov/PycharmProjects/pyOCRtext1/temp/test1.png')
-    # получаем строку
-    string = pytesseract.image_to_string(image,lang = 'rus')
-    # печатаем
-    print(string)
+    #filename = r'''temp\example_01.png'''
+    image = r'''temp\111.png'''
+
+    #preprocess = "thresh"
+    preprocess = "blur"
+
+    # загрузить образ и преобразовать его в оттенки серого
+    image = cv2.imread(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # проверьте, следует ли применять пороговое значение для предварительной обработки изображения
+
+    if preprocess == "thresh":
+        gray = cv2.threshold(gray, 0, 255,
+                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # если нужно медианное размытие, чтобы удалить шум
+    elif preprocess == "blur":
+        gray = cv2.medianBlur(gray, 3)
+
+    # сохраним временную картинку в оттенках серого, чтобы можно было применить к ней OCR
+
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    # загрузка изображения в виде объекта image Pillow, применение OCR, а затем удаление временного файла
+    text = pytesseract.image_to_string(Image.open(filename))
+    #os.remove(filename)
+    print(text)
+
+    # показать выходные изображения
+    cv2.imshow("Image", image)
+    cv2.imshow("Output", gray)
+    #input(‘pause…’)
 
 if __name__ == '__main__':
     main()
